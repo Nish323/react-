6028628251c6e6eef16a27c6a7e19d1b1555fe2d
Nish323 <doragon.nini@icloud.com>
2025-02-20 01:33:02 +0900

    useStateの導入

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,20 @@
+import { useState } from "react";
 import { ColorfullMessage } from "./ColorfullMessage";
 
 export const App = () => {
-  const onClickButton = () => alert();
+  const [num, setNum] = useState(0); //const [実際の値, 更新関数] = useState(初期値);
+  const onClickCountUp = () => {
+    setNum((prev) => prev + 1);
+    setNum((prev) => prev + 1);
+  };
 
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは</h1>
       <ColorfullMessage color="blue">お元気ですか？</ColorfullMessage>
       <ColorfullMessage color="green">元気です！</ColorfullMessage>
-      <button onClick={onClickButton}>ボタン</button>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+      <p>{num}</p>
     </>
   );
 };
